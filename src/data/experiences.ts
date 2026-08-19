e410b76f89f19cedae31689ee6207cdeca52db06
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2026-08-19 18:17:12 -0300

    feat: add new experience entry for Two Of Us and update periods for previous roles

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -9,10 +9,19 @@ export type Experience = {
 
 export const experiences: Experience[] = [
   {
+    id: 'tou',
+    company: 'Two Of Us',
+    role: 'Partner e Desenvolvedor Front-End',
+    period: 'Mai 2026 - Atualmente',
+    description:
+      'Partner e Front-End developer na empresa de tecnologia Two Of Us BR (TOU), atuando no desenvolvimento de um sistema ERP e um CRM WhatsApp-first. No dia a dia, transformo fluxos de atendimento e processos complexos em telas simples de usar com React, TypeScript e Next.js, além de criar e manter componentes reutilizáveis e escaláveis para o sistema. Também sou responsável por implementar integrações com APIs externas, garantindo que os dados fluam de forma eficiente entre diferentes sistemas.',
+    tags: ['React', 'TypeScript', 'CRM', 'UI|UX'],
+  },
+  {
     id: 'ipr',
     company: 'Instituto Pedro Ruiz',
     role: 'Desenvolvedor Front-End',
-    period: 'Jul 2025 - Atualmente',
+    period: 'Jul 2025 - Mai 2026',
     description:
       'Operar no desenvolvimento do CRM próprio do instituto, fui responsável por todo o desenvolvimento do Front-End do sistema desde o Design UI|UX das telas até a produção e integração com as APIs do backend. Além da manutenção dos outros sistemas web utilizados dentro da empresa, como o site institucional e landing pages.',
     tags: ['React', 'TypeScript', 'CRM', 'UI|UX'],
@@ -21,7 +30,7 @@ export const experiences: Experience[] = [
     id: 'prefeitura',
     company: 'Prefeitura Municipal de Santo André/SP',
     role: 'Desenvolvedor Frontend',
-    period: 'Mai 2024 - Atualmente',
+    period: 'Mai 2024 - Jul 2025',
     description:
       ' Responsável por operar na criação do conteúdo, estilos, layout, design interativo, funcionalidades e integrações com API dos Sistemas Web da Prefeitura de Santo André. Suporte e manutenção de bugs dos sites apresentados pelos setores da saúde, RH, segurança, frotas, além da vivência e prática atuando sob Metodologias Ágeis (SCRUM e KANBAN).',
     tags: ['JavaScript', 'HTML5', 'CSS3', 'SCRUM'],
@@ -30,7 +39,7 @@ export const experiences: Experience[] = [
     id: 'endolife',
     company: 'Endolife HealthTech',
     role: 'Web Developer e UI Designer',
-    period: 'Fev 2024 - Ago 2024',
+    period: 'Jan 2024 - Mai 2024',
     description:
       'Webdesign e desenvolvimento de Site Intitucional + Landing Pages de alta conversão para empresa, incluindo a criação do conteúdo, estilos, interatividades, layout e design não só interativo do site, mas também responsivo sendo adaptativo para tablets e celulares.  ',
     tags: ['React', 'Landing Pages', 'E-commerce'],
